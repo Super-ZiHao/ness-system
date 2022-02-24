f27bfeeb819a637a11dc2a604ed182e4a42c06ce
Mr.黄 <1106720577@qq.com>
2022-02-24 19:57:09 +0800

    完善侧边栏机制

--- src/components/sandbox/SideMenu.tsx
+++ src/components/sandbox/SideMenu.tsx
@@ -1,64 +1,77 @@
 import { Layout, Menu } from 'antd'
-import { UserOutlined } from '@ant-design/icons'
 import { MenuListType } from '@/types'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useLocation, useNavigate } from 'react-router-dom'
 
-// 模拟数组结构
-const menuList: MenuListType = [
-  {
-    key: '/home',
-    title: '首页',
-    icon: <UserOutlined />,
-  },
-  {
-    key: '/user-manage',
-    title: '权限管理',
-    icon: <UserOutlined />,
-    children: [
-      {
-        key: '/user-manage/role/list',
-        title: '角色列表',
-        icon: <UserOutlined />,
-      },
-      {
-        key: '/user-manage/right/list',
-        title: '用户列表',
-        icon: <UserOutlined />,
-      },
-    ],
-  },
-]
-
+const { Sider } = Layout
+const { SubMenu } = Menu
 const SideMenu = () => {
-  const { Sider } = Layout
-  const { SubMenu } = Menu
+  const navigate = useNavigate()
+  const location = useLocation()
+  const [menu, setMenu] = useState<any>()
+  const openKeys = [`/${location.pathname.split('/')[1]}`]
+  const selectKeys = [location.pathname]
+  useEffect(() => {
+    axios.get('http://localhost:8000/rights?_embed=children').then((res) => {
+      setMenu(res.data)
+    })
+  }, [])
+  // 判断返回的数据 sider 数据是否能被渲染
+  const chekPagePermission = (item: any) => {
+    return item.pagepermisson === 1
+  }
   // 循环生成 侧边栏列表
   const loopSidebar = (list: MenuListType) => {
     return (
-      <>
-        {list.map((item) => {
-          if (item.children && item.children.length > 0) {
-            return (
-              <SubMenu key={item.key} icon={item.icon} title={item.title}>
-                {loopSidebar(item.children)}
-              </SubMenu>
-            )
-          } else {
-            return (
-              <Menu.Item key={item.key} icon={item.icon}>
-                {item.title}
-              </Menu.Item>
-            )
-          }
-        })}
-      </>
+      menu && (
+        <>
+          {list.map((item) => {
+            if (
+              item.children &&
+              item.children.length > 0 &&
+              chekPagePermission(item)
+            ) {
+              return (
+                <SubMenu key={item.key} icon={item.icon} title={item.title}>
+                  {loopSidebar(item.children)}
+                </SubMenu>
+              )
+            } else {
+              return (
+                chekPagePermission(item) && (
+                  <Menu.Item
+                    key={item.key}
+                    icon={item.icon}
+                    onClick={() => {
+                      navigate(item.key)
+                    }}
+                  >
+                    {item.title}
+                  </Menu.Item>
+                )
+              )
+            }
+          })}
+        </>
+      )
     )
   }
   return (
     <Sider className="layout-sider">
-      <div className="logo">全球新闻发布管理系统</div>
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-        {loopSidebar(menuList)}
-      </Menu>
+      <div className="flex column" style={{ height: '100%' }}>
+        <div className="logo">全球新闻发布管理系统</div>
+        <Menu
+          className="w-full"
+          theme="dark"
+          mode="inline"
+          defaultOpenKeys={openKeys}
+          selectedKeys={selectKeys}
+          style={{ overflow: 'auto' }}
+        >
+          {loopSidebar(menu)}
+        </Menu>
+      </div>
     </Sider>
   )
 }
